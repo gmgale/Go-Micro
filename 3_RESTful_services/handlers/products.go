784e7b77435e11e458d2836866fce5e2fb6222b2
gmgale <gmgale@icloud.com>
2020-11-29 19:39:05 +0100

    3_RESTful_services complete

---
+++ 3_RESTful_services/handlers/products.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gmgale/go_micro/3_RESTful_services/data"
+)
+
+type Products struct {
+	l *log.Logger
+}
+
+func NewProducts(l *log.Logger) *Products {
+	return &Products{l}
+}
+
+func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	if r.Method == http.MethodGet {
+		p.getProducts(rw, r)
+		return
+	}
+
+	// handle an update
+
+	// catch all
+	rw.WriteHeader(http.StatusMethodNotAllowed)
+}
+
+func (p *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
+	lp := data.GetProducts()
+	err := lp.ToJSON(rw)
+	if err != nil {
+		http.Error(rw, "Unable to marshal JSON", http.StatusInternalServerError)
+	}
+}
