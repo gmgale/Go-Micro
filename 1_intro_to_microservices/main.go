12d79a437edde293ba5027c89cf69f017eeab28d
gmgale <gmgale@icloud.com>
2020-11-29 13:01:15 +0100

    1_intro_to_microservices complete ✓

--- 1_intro_to_microservices/main.go
+++ 1_intro_to_microservices/main.go
@@ -1,13 +1,22 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
 )
 
 func main() {
-	http.HandleFunc("/", func(http.ResponseWriter, *http.Request) {
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
 		log.Println("Hello World")
+		d, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "oops", http.StatusBadRequest)
+			return
+		}
+
+		fmt.Fprintf(rw, "Hello %s", d)
 	})
 
 	http.ListenAndServe(":9090", nil)
