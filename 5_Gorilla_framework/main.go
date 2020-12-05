7fe7a99861d8d561e40029c69485ddf391e8dc25
gmgale <gmgale@icloud.com>
2020-12-05 12:52:13 +0100

    5_Gorilla_framework complete

--- 5_Gorilla_framework/main.go
+++ 5_Gorilla_framework/main.go
@@ -7,7 +7,8 @@ import (
 	"os/signal"
 	"time"
 
-	"github.com/gmgale/go_micro/4_RESTful_services/handlers"
+	"github.com/gmgale/go_micro/5_Gorilla_framework/handlers"
+	"github.com/gorilla/mux"
 	"golang.org/x/net/context"
 )
 
@@ -15,8 +16,18 @@ func main() {
 	l := log.New(os.Stdout, "product-api", log.LstdFlags)
 	ph := handlers.NewProducts(l)
 
-	sm := http.NewServeMux()
-	sm.Handle("/", ph)
+	sm := mux.NewRouter()
+
+	getRouter := sm.Methods(http.MethodGet).Subrouter()
+	getRouter.HandleFunc("/", ph.GetProducts)
+
+	putRouter := sm.Methods(http.MethodPut).Subrouter()
+	putRouter.HandleFunc("/{id:[0-9]+}", ph.UpdateProducts)
+	putRouter.Use(ph.MiddlewareValidateProduct)
+
+	postRouter := sm.Methods(http.MethodPost).Subrouter()
+	postRouter.HandleFunc("/", ph.AddProduct)
+	postRouter.Use(ph.MiddlewareValidateProduct)
 
 	s := http.Server{
 		Addr:         ":9090",
